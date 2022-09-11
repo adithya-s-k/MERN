3325903854b6967fb954574853a3692461b1da82
Adithya S K <adithyaskolavi@gmail.com>
2022-09-11 12:18:51 +0530

    sign in with google set up complete - Adithya S K

--- React_setup/chatroom/src/App.js
+++ React_setup/chatroom/src/App.js
@@ -4,6 +4,9 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { userCollectionData } from 'react-firebase-hooks/firestore';
+
 firebase.initializeApp({
   apiKey: "AIzaSyBYsNielkC403yUP5SHy48U-dKcWCRUJl4",
   authDomain: "chatroom-745eb.firebaseapp.com",
@@ -14,14 +17,47 @@ firebase.initializeApp({
   measurementId: "G-N5R45CWCSS"
 });
 
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hi</h1>
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+
     </div>
   );
 }
 
+
+function SignIn(){
+  
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  
+  return(
+    <button onClick={signInWithGoogle}>Sign in with google</button>
+    );
+  }
+
+  function SignOut(){
+    return auth.currentUser &&(
+      <button onClick={()=>auth.signOut()} >Sign Out</button>
+    );
+  }
+  
+  function ChatRoom(){}
+  
 export default App;
