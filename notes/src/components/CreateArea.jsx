4b3b64e1c301a37c8a16bef4491289bcc35078fa
Adithya S K <adithyaskolavi@gmail.com>
2022-10-05 20:39:19 +0530

    add button added - Adithya S K

--- notes/src/components/CreateArea.jsx
+++ notes/src/components/CreateArea.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
+import { useState } from 'react';
 
 function CreateArea(props){
+
+    const [note, setNote] = useState({
+        title:"",
+        content:""
+    });
+
+    function handleChange(e){
+        const{name,value} = e.target
+        setNote(preValue =>{
+            return{
+                ...preValue,
+                [name]:value
+            }
+        })
+    }
+
+    function submitButton(i){
+        i.preventDefault()
+        console.log(i.target)
+    }
+
     return(
         <div>
             <form>
-                <input type="text" placeholder="Title..." name="title" />
+                <input 
+                    type="text" 
+                    placeholder="Title..." 
+                    name="title" 
+                    value={note.title}
+                    onChange={handleChange}
+                    />
                 <p>
-                    <textarea name="content" placeholder="Take a note.."></textarea>
+                    <textarea 
+                        name="content" 
+                        placeholder="Take a note.." 
+                        value={note.content} 
+                        onChange={handleChange} >
+                    </textarea>
                 </p>
+                <button onClick={submitButton}>Add</button>
             </form>
         </div>
     );
