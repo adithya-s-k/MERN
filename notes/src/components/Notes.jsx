c0816f3a7e4e3ee808d1aebaa71fcc2bfaf9c9b6
Adithya S K <adithyaskolavi@gmail.com>
2022-10-05 20:10:29 +0530

    notes component created - Adithya S K

---
+++ notes/src/components/Notes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Notes(props){
+    return(
+        <>
+            <div className="note">
+                <h1>My Note</h1>
+                <p></p>
+            </div>
+        </>
+    );
+}
+export default Notes;
