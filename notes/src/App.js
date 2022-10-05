ce90af711535eac095385fce69ccf78bcab0ca71
Adithya S K <adithyaskolavi@gmail.com>
2022-10-05 20:03:05 +0530

    create component added - Adithya S K

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import './styles.css'
+
 import Header from './components/Header'
+import CreateArea from './components/CreateArea'
+import Notes from './components/Notes'
 
 function App() {
   return (
     <>
       <div>
         <Header />
+        <CreateArea />
+        <Notes />
       </div>
     </>
   );
