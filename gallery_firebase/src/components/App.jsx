48132125c603469290182eff8d5b4986da01190d
Adithya S K <adithyaskolavi@gmail.com>
2022-10-08 21:40:45 +0530

    photo gallery semi working - Adithya S K

--- gallery_firebase/src/components/App.jsx
+++ gallery_firebase/src/components/App.jsx
@@ -1,9 +1,24 @@
+import React from 'react';
+import { BrowserRouter,Route,Routes  } from 'react-router-dom'
 import './App.css';
+import Navbar from './Navbar'
+import Upload from './Upload'
+// import Photos from './Photos'
+// import Progress from './Progress'
+import Home from './Home'
 
 function App() {
   return (
     <>
-      <h2>Hello</h2>
+      <BrowserRouter>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="upload" component = {Upload}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
